8ac6757337755ba8ea6e8f050fa029e61de88357
Dan Powell <powell956@gmail.com>
2021-06-08 17:00:58 -0400

    commit tree 6903bacd1022c189aa9da49b48ff2512478eeba3

---
+++ lib/artist.rb
@@ -0,0 +1,41 @@
+class Artist
+    extend Concerns::Findable 
+
+    attr_accessor :name
+
+    @@all =[]
+
+    def initialize(name)
+        @name = name
+    end
+
+    def self.all 
+        @@all 
+    end
+
+    def save
+        self.class.all << self
+        self
+    end
+
+    def self.destroy_all
+        all.clear
+    end
+
+    def self.create(name)
+        new(name).save
+    end
+
+    def add_song(song)
+        song.artist = self unless song.artist
+    end
+
+    def songs
+        Song.all.select{|s| s.artist ==  self}
+    end
+
+    def genres
+        songs.collect{|s| s.genre}.uniq
+    end
+
+end
